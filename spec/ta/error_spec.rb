9426c3c76d935df7b76b1fa7caf0ce06e49d898d
Ben Williams <8enwilliams@gmail.com>
2019-11-18 17:32:27 +1100

    error spec

--- spec/ta/error_spec.rb
+++ spec/ta/error_spec.rb
@@ -1,6 +1,17 @@
 RSpec.describe TA::Error do
   describe '.new' do
-    skip 'sets enum and info'
-    skip 'formats the message string using the enum and info strings'
+    subject { TA::Error.new(1) }
+
+    it 'sets enum' do
+      expect(subject.enum).to eq 'TA_SUCCESS'
+    end
+
+    it 'sets info' do
+      expect(subject.info).to eq 'No error'
+    end
+
+    it 'formats the message string using the enum and info strings' do
+      expect(subject.message).to eq 'TA_SUCCESS: No error'
+    end
   end
 end
